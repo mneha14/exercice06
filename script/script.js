3aad601273688c8cd2771df202339ddeca234486
Marianne Nobert <mariannenobert@hotmail.com>
2017-02-14 16:11:43 -0500

    exercice 05 terminé

--- script/script.js
+++ script/script.js
@@ -1,38 +1,54 @@
 
 	// on cible l'élément « path » que l'on veut animer 
-	var triangle = document.querySelectorAll("#monSVG path")[0];
+	var formes = document.querySelectorAll("#monSVG path");
+console.log(formes.length);
+	var longeurTableau = new Array();
 	// la longueur du chemin «path»
-	var longueur = triangle.getTotalLength();
+	for(i=0; i<formes.length;i++){
 
-	console.log(longueur)
+	var longeur = formes[i].getTotalLength();
+
+	longeurTableau[i] = longeur;
 
 	// La longueur du tracé
 	// https://developer.mozilla.org/en/docs/Web/SVG/Attribute/stroke-dasharray
 
-	triangle.style.strokeDasharray = longueur;
+	formes[i].style.strokeDasharray = longeur;
+	
 
 	// On fait disparaître le tracé en appliquant la propriété css strokeDashoffset sur toute la longueur du chemin
-	triangle.style.strokeDashoffset = longueur;
+	formes[i].style.strokeDashoffset = longeur;
+
+	}
+
+	
 
 	// extraire la fraction du déplacement de la barre de défilement et  ajusté le tracé
 	window.addEventListener("scroll", maTrace);
 
 	function maTrace() {
 
-		console.log('scrollTop de body = ' + document.body.scrollTop)
+		/*console.log('scrollTop de body = ' + document.body.scrollTop)
 		console.log('scrollTop de documentElement = ' + document.documentElement.scrollTop)
 		console.log('scrollHeight de documentElement = ' + document.documentElement.scrollHeight)	
-		console.log('clientHeight de documentElement = ' + document.documentElement.clientHeight)			
+		console.log('clientHeight de documentElement = ' + document.documentElement.clientHeight)*/			
 
 https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight
 
 
+console.log(formes.length);
+	  
+	  for(i=0; i<formes.length;i++){
 
-	  var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+		var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
-	  var trace = longueur * fractionDuScroll;
+	  	var trace = longeurTableau[i] * fractionDuScroll;
 
 	  // tracé selon la fraction de «scroll»
-	  triangle.style.strokeDashoffset = longueur - trace ;
+	  formes[i].style.strokeDashoffset = longeurTableau[i] - trace ;
+
+	  }
+
+	  
 	}
 	
